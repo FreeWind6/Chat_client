c021b445872210b4c8d5a94e3c868cd1a6266b4b
Dmitry <free-wind-2012@yandex.ru>
2020-01-24 11:49:58 +0300

    first commit

---
+++ src/client/Controller.java
@@ -0,0 +1,222 @@
+package client;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import javax.swing.text.Style;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+public class Controller {
+/*    @FXML
+    TextArea textArea;*/
+
+    @FXML
+    TextField textField;
+
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+
+    final String IP_ADPRESS = "localhost";
+    final int PORT = 8189;
+
+    @FXML
+    HBox upperPanel;
+
+    @FXML
+    HBox bottomPanel;
+
+    @FXML
+    TextField loginField;
+
+    @FXML
+    PasswordField passwordField;
+
+    private boolean isAuthorized;
+
+    @FXML
+    ListView<String> clientList;
+
+    @FXML
+    ListView messagesView;
+    String nick = "";
+
+    public void setMsg(String str) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                Label message = new Label(str);
+                VBox messageBox = new VBox(message);
+                if (nick != "") {
+                    String[] mass = str.split(":");
+                    if (nick.equalsIgnoreCase(mass[0])) {
+                        messageBox.setAlignment(Pos.CENTER_RIGHT);
+                    }
+                }
+                messagesView.getItems().add(messageBox);
+            }
+        });
+    }
+
+    public void setAuthorized(boolean isAuthorized) {
+        this.isAuthorized = isAuthorized;
+        if (!isAuthorized) {
+            upperPanel.setVisible(true);
+            upperPanel.setManaged(true);
+            bottomPanel.setVisible(false);
+            bottomPanel.setManaged(false);
+            clientList.setVisible(false);
+            clientList.setManaged(false);
+        } else {
+            upperPanel.setVisible(false);
+            upperPanel.setManaged(false);
+            bottomPanel.setVisible(true);
+            bottomPanel.setManaged(true);
+            clientList.setVisible(true);
+            clientList.setManaged(true);
+        }
+    }
+
+
+    public void connect() {
+        try {
+            socket = new Socket(IP_ADPRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.startsWith("/authok")) {
+                                String[] mass = str.split(" ");
+                                nick = mass[1];
+                                setAuthorized(true);
+                                break;
+                            } else {
+                                setMsg(str);
+                            }
+                        }
+
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.equals("/serverclosed")) break;
+                            if (str.startsWith("/clientlist")) {
+                                String[] tokens = str.split(" ");
+                                Platform.runLater(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        clientList.getItems().clear();
+                                        for (int i = 1; i < tokens.length; i++) {
+                                            clientList.getItems().add(tokens[i]);
+                                        }
+                                    }
+                                });
+                            } else {
+                                setMsg(str);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        setAuthorized(false);
+                    }
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg() {
+        try {
+            out.writeUTF(textField.getText());
+            textField.clear();
+            textField.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tryToAuth(ActionEvent actionEvent) {
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+        try {
+            out.writeUTF("/auth " + loginField.getText() + " " + passwordField.getText());
+            loginField.clear();
+            passwordField.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Dispose() {
+        System.out.println("Отправляем сообщение на сервер о завершении работы");
+        try {
+            if (out != null) {
+                out.writeUTF("/end");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void info() throws IOException {
+//        Image imageInfo = new Image("file:images/info.jpg");
+        Stage infoWindow = new Stage();
+
+        Parent rootInfo = FXMLLoader.load(getClass().getResource("info.fxml"));
+        infoWindow.setTitle("Info");
+//        infoWindow.getIcons().add(imageInfo);
+        infoWindow.setScene(new Scene(rootInfo, 550, 100));
+        infoWindow.show();
+    }
+
+    public void about() throws IOException {
+//        Image imageInfo = new Image("file:images/about.png");
+        Stage infoWindow = new Stage();
+
+        Parent rootInfo = FXMLLoader.load(getClass().getResource("about.fxml"));
+        infoWindow.setTitle("About");
+//        infoWindow.getIcons().add(imageInfo);
+        infoWindow.setScene(new Scene(rootInfo, 350, 100));
+        infoWindow.show();
+    }
+
+    public void clearWindow() {
+        messagesView.getItems().clear();
+    }
+}
